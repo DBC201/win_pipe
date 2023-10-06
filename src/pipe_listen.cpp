1952afe3839c33a5ce84f80c02f4a2c94c0b14e3
dbc201 <denizbcakiroglu@gmail.com>
2023-10-06 16:16:29 -0400

    fix example

--- src/pipe_listen.cpp
+++ src/pipe_listen.cpp
@@ -6,28 +6,28 @@ using pipe_utils::ByteArray;
 int main() {
     const char* pipeName = "\\\\.\\pipe\\sdn_pipe";
 
-	ChildPipe childPipe(pipeName);
-	// while (1) {
-	// 	ByteArray binaryData = childPipe.read();
+	ChildPipe childPipe(pipeName, true, false);
+	while (1) {
+		ByteArray binaryData = childPipe.read();
 
-	// 	if (binaryData.empty()) {
-	// 		std::cout << "No data received" << std::endl;
-	// 		break;
-	// 	}
+		if (binaryData.empty()) {
+			std::cout << "No data received" << std::endl;
+			break;
+		}
 
-	// 	// for (auto byte : binaryData) {
-	// 	// 	std::cout << std::hex << static_cast<int>(byte) << " ";
-	// 	// }
+		// for (auto byte : binaryData) {
+		// 	std::cout << std::hex << static_cast<int>(byte) << " ";
+		// }
 
-	// 	std::cout << std::endl;
+		std::cout << std::endl;
 		
-	// 	std::cout << "Received data: ";
+		std::cout << "Received data: ";
 
-	// 	std::cout << std::dec << binaryData.size() << " bytes" << std::endl;
+		std::cout << std::dec << binaryData.size() << " bytes" << std::endl;
 
-	// 	std::cout << "---------------------------------------------" << std::endl;
+		std::cout << "---------------------------------------------" << std::endl;
 
-	// }
+	}
 
     return 0;
 }
